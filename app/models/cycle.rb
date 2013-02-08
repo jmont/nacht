7d6de5c86cf060804fe107246ca7e790ed4650f6
Juan Carlos Montemayor Elosua <j.mont@me.com>
2013-02-07 20:20:26 -0500

    Added Cycle model

---
+++ app/models/cycle.rb
@@ -0,0 +1,3 @@
+class Cycle < ActiveRecord::Base
+  attr_accessible :composer, :name
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130208011850) do
+ActiveRecord::Schema.define(:version => 20130208012010) do
+
+  create_table "cycles", :force => true do |t|
+    t.string   "name"
+    t.string   "composer"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "songs", :force => true do |t|
     t.string   "name"
